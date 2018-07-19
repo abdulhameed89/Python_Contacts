df9273980c74ebda1db425a32e0aa71d4b6cae36
abdulhameed89 <abd.cs@hotmail.com>
2018-07-19 21:06:13 +0300

    send the response to the server

--- part1-Basic Python Server/requestHandler.py
+++ part1-Basic Python Server/requestHandler.py
@@ -1,18 +1,19 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
  #This class will handle any incoming request from
- #the browser 
+ #the browser
 
 class myHandler(BaseHTTPRequestHandler):
 	#This will handle GET requests
 	def do_GET(self):
 		# Add the outgoing status here
-		
+		self.send_response(200)
 		# You will need to change this if you are sending something
   		# other than plain text, like JSON or HTML.
 		self.send_header('Content-type','text/html')
 		self.end_headers()
+		self.wfile.write("Hello World")
 
-		# Send the "Hello World" message here 
-		
+		# Send the "Hello World" message here
+
 		return
